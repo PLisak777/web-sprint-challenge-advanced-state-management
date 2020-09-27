a3205c07878e0fcccc78517b5d603637a3cdcfb2
Phil <philip-lisak@lambdastudents.com>
2020-09-27 15:30:06 -0700

    Store is completed and implemented on index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,11 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import { createStore, applyMiddleware } from 'redux';
-// import { Provider } from 'react-redux';
-// import { smurfReducer } from ''
-import "./index.css";
-import App from "./components/App";
+import React, { useContext } from 'react';
+import ReactDOM from 'react-dom';
 
-// const 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { smurfReducer } from './reducers/smurfReducer';
+
+import thunk from 'redux-thunk';
+
+import './index.css';
+import App from './components/App';
+
+const store = createStore(smurfReducer, applyMiddleware(thunk));
+console.log('pl: index.js: store: ', store);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,15 @@
+const initialState = [
+	{
+		name: '',
+		age: 200,
+		height: '',
+		id: 0,
+	},
+];
+
+export const smurfReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
