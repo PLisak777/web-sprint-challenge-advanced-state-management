1991ebb64d03ec4126507818bf01146e08dda31a
Phil <philip-lisak@lambdastudents.com>
2020-09-27 15:10:19 -0700

    Starting over

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import { smurfReducer } from ''
 import "./index.css";
 import App from "./components/App";
+
+// const 
 
 ReactDOM.render(<App />, document.getElementById("root"));
